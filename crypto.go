f0f0fb7baa1c63e937955d6914096b419dea572e
Clifford Richardson <cmrallen@gmail.com>
2017-02-19 12:26:32 -0800

    Made the cipher creation more prim and proper

--- crypto.go
+++ crypto.go
@@ -3,12 +3,21 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"math/big"
 )
 
 var secret []byte
 
-var globalCipher cipher.Block
+func createNewKey() ([]byte, *big.Int, *big.Int, error) {
+	return elliptic.GenerateKey(elliptic.P521(), rand.Reader)
+}
 
-func init() {
-	globalCipher, _ = aes.NewCipher(secret)
+func createNewCipher(priv []byte, slaveX, slaveY *big.Int) (cipher.AEAD, error) {
+	sharedSecretX, sharedSecretY := elliptic.P521().ScalarMult(slaveX, slaveY, priv)
+	hash := sha256.Sum256(append(sharedSecretX.Bytes(), sharedSecretY.Bytes()...))
+	block, _ := aes.NewCipher(hash[:])
+	return cipher.NewGCM(block)
 }
