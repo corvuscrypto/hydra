ca8ee81541c4993cc4fa3d413e97afceb3f8bc73
Clifford Richardson <cmrallen@gmail.com>
2017-02-18 22:49:12 -0800

    Added monitor file and some code related to a global monitor

---
+++ monitor.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+//MonitorChanBufSize is the max buffer size for the channel used in the monitor
+const MonitorChanBufSize = 1 << 16
+
+var eventCount uint64
+
+//go:generate stringer -type=eventType
+type eventType uint8
+
+//Event Types
+const (
+	EventIncomingRequest eventType = iota
+	EventRequestForward
+	EventSlaveOffer
+	EventSlaveAccept
+	EventSlaveTimeout
+	EventSlaveError
+	EventPanic
+	EventRestart
+	EventShutdown
+)
+
+type event struct {
+	ID        uint64
+	Type      eventType
+	Timestamp time.Time
+	Source    string
+	Details   string
+}
+
+//NewEvent creates a new event
+//NOTE: possible refactor
+func NewEvent(t eventType, source, details string) (evt *event) {
+	evt.Type = t
+	evt.Timestamp = time.Now()
+	evt.ID = eventCount
+	atomic.AddUint64(&eventCount, uint64(1))
+	return
+}
+
+type monitor struct {
+	channel     chan *event
+	EventLog    []*event
+	EventCounts map[eventType]uint
+}
+
+func (m *monitor) waitForEvents() {
+	for {
+		e := <-m.channel
+		m.EventLog = append(m.EventLog, e)
+		m.EventCounts[e.Type]++
+	}
+}
+
+func (m monitor) addEvent(e *event) {
+	m.channel <- e
+}
+
+var globalMonitor = new(monitor)
+
+func init() {
+	//init the vars for the global monitor instance
+	globalMonitor.channel = make(chan *event, MonitorChanBufSize)
+	globalMonitor.EventCounts = make(map[eventType]uint)
+}
